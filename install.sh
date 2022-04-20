9f5db5af4c4b700a350dc8d89653502d96f82fa9
Anton Christoffersson <anton_christoffersson@hotmail.com>
2022-04-20 19:49:12 +0200

    Install script

--- install.sh
+++ install.sh
@@ -1,17 +1,12 @@
-* Dotfiles
-
-#+BEGIN_SRC bash
+#!/bin/bash
 pacman -S xorg plasma konsole dolphin neovim emacs git firefox openssh base-devel kdeconnect
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 git clone /opt/ && cd /opt/yay/ && makepkg -si
 yay -S nvim-packer-git
-#+END_SRC
-
-#+BEGIN_SRC bash
+# Setup Links
 ln -s ~/.dotfiles/doom.d ~/.doom.d
 ln -s ~/.dotfiles/nvim ~/.config/nvim
-#+END_SRC
-
-#+BEGIN_SRC bash
+# Install doom
 ~/.emacs.d/bin/doom install
-#+END_SRC
+
+
