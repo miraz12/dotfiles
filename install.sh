54e84a059b8f1f8a0f7595864eb718b2a5cf5c97
Anton Christoffersson <anton_christoffersson@hotmail.com>
2022-05-07 17:32:27 +0200

    Fixed install script and switched to paru

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 codeblock=0
 while read line; 
 do 
-	if [[ $line == *"END_SRC bash"* ]]; then
+	if [[ $line == *"END_SRC"* ]]; then
 		codeblock=0
 	fi
-	if [[ codeblock == 1 ]]; then
-		$line; 
+	if [[ $codeblock == 1 ]]; then
+		echo $line; 
 	fi
 	if [[ $line == *"BEGIN_SRC bash"* ]]; then
 		codeblock=1
