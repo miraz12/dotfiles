04f59c16032cc48d46c531f66537fc1a3fc017aa
Anton <anton_christoffersson@hotmail.com>
2023-01-20 15:07:23 +0100

    Switch to sway

--- xmonad/scripts/layout_switch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-total_layouts="${#@}"
-current_layout=$(setxkbmap -query | awk '/layout:/{ print $2 }')
-current_index="${@[(i)$current_layout]}"
-next_index="$((current_index % total_layouts + 1))"
-next_layout="${@[$next_index]}"
-
-setxkbmap "$next_layout"
